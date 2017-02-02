0c3c8e525461be0c65ea27205ed679860b09b37b
Naaach <nach704@gmail.com>
2017-02-02 11:32:15 +0100

    Add files via upload

--- js/animations.js
+++ js/animations.js
@@ -1,15 +1,95 @@
 
+	//Screen width correction on DOM
+	sWidth = screen.width;
+ 	document.getElementById('body').style.maxWidth = sWidth;
+
+
+
+
 $(document).ready(function(){
 	//JQuery Start
+	var $this = $(this);
 
-	/*New table element fade in (beta) 
-	Only for first-chilfren element*/
-	var $newTableElement = $("#masterTableItems").children("tr");
- 	
-	//cambiar la opcacidad a 0 a la creacion del elemento (item)
-	$newTableElement.click(function(){
-		alert("clikc en tr");
+
+
+	//hidden and toogle pop-up form
+	var $popUpForm = $('#pop-up-form-grandfather');
+	$popUpForm.hide();
+
+	var $overlayPopUp = $('.overlay-form');
+	$overlayPopUp.hide();
+	
+	var $closePopUp = $('#close-pop-up > i');
+	var $buttomForm = $('#open-add');
+
+	var $tb1 = $('#tool-button-1');
+	var $tb2 = $('#tool-button-2');
+	var $tb3 = $('#tool-button-3');
+
+	//Open
+	$buttomForm.click(function(){
+
+		$overlayPopUp.fadeTo(250, 1, function(){
+			$(this).show();
+		});
+		$popUpForm.delay(250).fadeTo(500, 1, function(){
+			$(this).show();
+		});
+
 	});
 
-	//Enda of JQuery
+	//close
+	$dissapear = function(){
+		$popUpForm.fadeTo(250, 0, function(){
+			$(this).hide();
+		});
+		$overlayPopUp.delay(250).fadeTo(500, 0, function(){
+			$(this).hide();
+		});
+
+	};
+
+	$overlayPopUp.click($dissapear);
+	$closePopUp.click($dissapear);
+
+
+	//#open-add animations
+	var $openAdd = $('#open-add');
+
+	$openAdd.hover(
+		//Mouseenter
+		function(){
+			$(this).animate({padding: "+=5.2px"}, 150);
+			$(this).children('i').animate({'font-size': "+=1px"}, 200);
+			
+			$tb1.delay(400).fadeTo(500, 1, function(){
+				$(this).show();
+			});
+			$tb2.delay(600).fadeTo(500, 1, function(){
+				$(this).show();
+			});
+			$tb3.delay(800).fadeTo(500, 1, function(){
+				$(this).show();
+			});
+
+		},
+		//Mouseleave
+		function(){
+
+			$tb1.delay(800).fadeTo(500, 0, function(){
+				$(this).hide();
+			});
+			$tb2.delay(600).fadeTo(500, 0, function(){
+				$(this).hide();
+			});
+			$tb3.delay(400).fadeTo(500, 0, function(){
+				$(this).hide();
+			});
+
+			$(this).animate({padding: "-=5.2px"}, 150);
+			$(this).children('i').animate({'font-size': "-=1px"}, 200);
+		}
+	);
+
+	//End of JQuery
 });
--- js/engine.js
+++ js/engine.js
@@ -1,48 +1,82 @@
 /*****  Naaach!  *****/
 
-(function(){
+//(function(){
 
 	//Variables
+//	validadorDatos = true;
+
 	//formulario
-	formDescripcion = document.getElementById("itemDesc"),
-	formTracking = document.getElementById("trackingNumber"),
-	formShop = document.getElementById("shopList"),
+	formDescripcion = document.getElementById("itemDesc");
+	formTracking = document.getElementById("trackingNumber");
+	formShop = document.getElementById("shopList");
 	formButton = document.getElementById("botonAgregar");
 	formButtonDel = document.getElementById("botonBorrarDB");
-	
-		
+
+
 
 	//Tabla de items
 	var masterTableItems = document.getElementById("masterTableItems"); /*<tbody>*/
 		removeButtons = document.getElementsByClassName("removeItemButton");
 
 	//Funciones
+
+	validadorCampos = function(){
+		formDescripcion = document.getElementById("itemDesc");
+		formTracking = document.getElementById("trackingNumber");
+		formShop = document.getElementById("shopList");
+
+		descripcion = formDescripcion.value,
+		trackingNumber = formTracking.value,
+		shop = formShop.value;
+		var valido = true;
+
+		if(descripcion===""){
+			valido = false;
+			formDescripcion.classList.add("alert-danger");
+			formDescripcion.placeholder = "Falta Descripcion";
+		}else{
+			formDescripcion.classList.remove("alert-danger");
+			formDescripcion.placeholder=" Descripcion";
+		}
+
+		if(trackingNumber===""){
+			valido = false;
+			formTracking.classList.add("alert-danger");
+			formTracking.placeholder = "Falta Nº Tracking";
+		}else{
+			formTracking.classList.remove("alert-danger");
+			formTracking.placeholder="Nº Tracking";
+		}
+
+		if((shop === "Selecciona Tienda") || (shop === "")){
+			valido = false;
+			formShop.classList.add("alert-danger");
+		}else{
+			formShop.classList.remove("alert-danger");
+		}
+
+		if (valido){
+			//limpieza de clases en campos
+			formDescripcion.classList.remove("alert-danger");
+			formTracking.classList.remove("alert-danger");
+			formShop.classList.remove("alert-danger");
+
+			//Nuvos placeholders
+			formDescripcion.placeholder=" Descripcion";
+			formTracking.placeholder="Nº Tracking";
+
+			newElement();	//llamada a la creacion de un nuevo elemento
+		}
+	};
+
 	newElement = function(){
+
 		descripcion = formDescripcion.value,
 		trackingNumber = formTracking.value,
 		shop = formShop.value;
 
 		x=document.createTextNode("x");
-		validadorDatos = true;
 
-		//validador de datos
-		if ((descripcion === "") || (trackingNumber === "") || ((shop === "Selecciona Tienda") || (shop === ""))) {
-			if (descripcion==="") {validadorDatos=false;formDescripcion.classList.add("alert-danger");formDescripcion.placeholder="Falta Descripcion";}else{descripcionInfo="";}
-			if (trackingNumber==="") {validadorDatos=false;formTracking.classList.add("alert-danger");formTracking.placeholder="Falta Nº Tracking";}else{trackingNumberInfo="";}
-			if ((shop === "Selecciona Tienda") || (shop === "")) {validadorDatos=false;formShop.classList.add("alert-danger");}else{shopInfo="";}
-			//validadorDatos
-			if (validadorDatos === false) {return validadorDatos;}else{validadorDatos = true;}
-		}
-
-		if (validadorDatos) {
-			formDescripcion.classList.remove("alert-danger");
-			formTracking.classList.remove("alert-danger");
-			formShop.classList.remove("alert-danger");
-
-			formDescripcion.placeholder=" Descripcion";
-			formTracking.placeholder="Nº Tracking";
-		}
-		
 		//crear elementos de nueva fila
 		item = document.createElement("tr");
 		item.setAttribute("class", "item");
@@ -86,22 +120,22 @@
 		item.appendChild(tdRMClone);
 		//masterTableItems
 		masterTableItems.appendChild(item);
-		
-		
+
+
 
 		//limpiar inputs de formulario
 		formDescripcion.value = "";
  		formTracking.value = "";
  		formShop.value = "Selecciona Tienda";
 
- 		//Evento de borrado
-		formButton.addEventListener("click", newElement);	//nuevo Elemento
-
+		//borrado selectivo
 		for (var i = 0; i < removeButtons.length; i++) {
 			removeButtons[i].addEventListener("click", removerItems);	//eliminar elemento
 		}
-
 	}
+
+	//Llamada al validador
+	formButton.addEventListener("click", validadorCampos);	//& nuevo Elemento
 
 	//Borrar elemento
 	 removerItems = function(){
@@ -109,65 +143,13 @@
 	}
 
 	//Eventos
-	formButton.addEventListener("click", newElement);	//nuevo Elemento
+//	formButton.addEventListener("click", newElement);	//nuevo Elemento
 	for (var i = 0; i < removeButtons.length; i++) {
 		removeButtons[i].addEventListener("click", removerItems);	//eliminar elemento
 	}
 
-	/**** indexedDB  ****/
-	
+	/**
+	*DATABASE HERE!
+	**/
 
-	//only database and self-functions¡
-	var indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
-	//create or open a DB
-	var open = indexedDB.open("DataBase");
-
-
-	//Schema
-	open.onsuccess = function(){
-		db = open.result;
-
-	};
-
-	open.onupgradeneeded = function (){
-		//Transactions
-		 db = open.result;
-		 store = db.createObjectStore("Codigos", {keyPath: "tracking"});
-		 tx = db.transaction("Codigos", "readwrite");
-		 store = tx.objectStore("Codigos");
-
-	 	//Close transaction
-		tx.oncomplete = function() {
-        	db.close();
-        };
-	};
-
-	//agregar objeto
-	newObject = function(){
-		db = open.result;
-		tx = db.transaction("Codigos", "readwrite");
-		store = tx.objectStore("Codigos");
-
-		//add data
-		store.add({tracking: trackingNumber, descripcion: descripcion, tienda: shop});
-
-
-    };
-	
-
-	//Triggers
-	formButton.addEventListener("click", newObject);
-
-	//clean DB
-	cleanObjectStorage = function(){
-		tx = db.transaction("Codigos", "readwrite");
-		objectStore = tx.objectStore("Codigos");
-		
-		objectStore.clear();
-
-
-	};
-
-	formButtonDel.addEventListener("click", cleanObjectStorage);
-	/**** /indexedDB  ****/
-}());
+//}());
